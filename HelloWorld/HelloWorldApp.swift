89a0e90931eb52656b04362aafa7ba64576da484
kyawlinnthant <kyawlinnthantkyawlinnthant@gmail.com>
2022-04-06 10:53:15 +0630

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by android developer on 06/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
