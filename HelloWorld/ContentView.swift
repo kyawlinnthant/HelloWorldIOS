2f424a9eb8fad771e6b418315eb61a2856143bc8
kyawlinnthant <kyawlinnthantkyawlinnthant@gmail.com>
2022-04-06 23:35:25 +0630

    navigation

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -9,15 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            Text("Hello, world!")
-                .padding()
-            Text("I am kyaw kyaw")
-                .padding()
+        NavigationView{
+            First()
         }
-    
     }
 }
+
+struct First : View{
+    var body: some View{
+        VStack{
+            Text("This is first").padding()
+            NavigationLink(destination: {
+                Second()
+            }, label: {
+                Text("Go to Second")
+            })
+            .navigationTitle("First View")
+        }
+    }
+}
+
+struct Second : View{
+    var body: some View{
+        VStack{
+            Text("This is second").padding()
+                .navigationTitle("Second View")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
